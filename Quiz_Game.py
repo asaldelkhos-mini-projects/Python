475992848c5f175367021a50683ed55428d456a5
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-30 20:28:38 +0430

    so here our questions and answers

--- Quiz_Game.py
+++ Quiz_Game.py
@@ -2,6 +2,30 @@ print("Welcome to my compuyter quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
-    
+print("Okey, Let's play <3")
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+else:
+    print("Incorrect!")
