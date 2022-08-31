ac787377aa88c95a3c91b6b71829e95529b4d7e7
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-31 17:00:25 +0430

    workig good

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -13,4 +13,16 @@ else:
     quit()
 
 random_number = random.randint(0, top_of_range)
-print(random_number)
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time")
+        continue
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrog!")
