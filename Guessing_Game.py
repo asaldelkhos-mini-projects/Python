981a5e68205d75d353e532fe1da0f6a19e05e5bc
asaldelkhosh <asaldelkhosh@gmail.com>
2022-08-31 16:47:39 +0430

    everything working good

---
+++ Guessing_Game.py
@@ -0,0 +1,16 @@
+import random # defalt module
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+
+    if top_of_range <= 0:
+        print("Please type a number larger than next time.")
+        quit()
+else:
+    print("Please type a number next time")
+    quit()
+
+random_number = random.randint(0, top_of_range)
+print(random_number)
