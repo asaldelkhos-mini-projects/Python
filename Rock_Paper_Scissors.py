9d30770a80b3a5af4a7fb4a7d441555600cabe7f
asaldelkhosh <asaldelkhosh@gmail.com>
2022-09-02 18:46:57 +0430

    get an error bc I wrote Ture instead of True

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -2,10 +2,16 @@ import random
 
 user_wins = 0
 computer_wins = 0
+options = ['rock', 'paper', 'scissors']
 
 while Ture:
     user_input = input("Type Rock/Paper/Scissors or 'q' to quit: ").lower()
     if user_input == 'q':
-        quit()
-    if user_input in ['rock', 'paper', 'scissors']:
-            
+        break
+
+    if user_input not in options:
+        continue
+    random_number = random.randint(0, 2)
+    # rock:0, paper:1, scissors:2
+    computer_pick = options[random_number]
+    prit("computer picked", computer_pick+ ".")
