ce4a64569ed340738d4e6fbb4c24dc30ee893f41
asaldelkhosh <asaldelkhosh@gmail.com>
2022-09-02 22:47:24 +0430

    from now we want to work on this things

--- password_manager .py
+++ password_manager .py
@@ -1,2 +1,11 @@
 # a little advanced one
 # Warning:  this is not a secure way, it's just for fun & practice
+
+pwd = input("What is the master password? ")
+
+def view():
+
+def add():
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones(view/new)? ")
