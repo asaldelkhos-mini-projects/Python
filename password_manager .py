98e3cf67b7e63f8c5228abd0ae0f273797355b6b
asaldelkhosh <asaldelkhosh@gmail.com>
2022-09-02 23:26:10 +0430

    until now we wrote the basics

--- password_manager .py
+++ password_manager .py
@@ -4,8 +4,28 @@
 pwd = input("What is the master password? ")
 
 def view():
+    with open('password.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print(f"User: {user}, Password: {passw}")
 
 def add():
+    name = input("Account name: ")
+    pws = input("Password: ")
+
+    with open('password.txt', 'a') as f:
+        f.write(name + "|" + pwd +  "\n")
+
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones(view/new)? ")
+    mode = input("Would you like to add a new password or view existing ones (view/add), or press 'q' to quit? ")
+    if mode == 'q':
+        break
+    elif mode == 'view':
+        view()
+    elif mode == 'add':
+        add()
+    else:
+        print("Invalid mode.")
+        continue
