9c8d7acdd31127f57f7c79e4afff928ccd5d86df
asaldelkhosh <asaldelkhosh@gmail.com>
2022-09-15 12:27:36 +0430

    this is the programm and now we check if it is work

--- merge_sort/MS.py
+++ merge_sort/MS.py
@@ -3,5 +3,29 @@ def merge_sort(arr):
         left_arr = arr [:len(arr)//2]
         right_arr = arr[len(arr)//2:]
 
+        # recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        # merge
+        i = 0 # left_arr idx
+        j = 0 # right_arr idx
+        k = 0 # merge array idx
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+            k += 1
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1    
 
 arr_test = [2, 3, 5, 1, 7, 4, 4, 4, 2, 6, 0]        
